67c8fcd4938fd1bdd53c55f019e6843326890c83
Lapinlc <lapinlc@outlook.com>
2023-07-13 20:45:38 -0600

    Proyecto en visual studio 2022

---
+++ PruebasC#/AplicacionDePrueba/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Consola
+{
+    public class ConsolaMain
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+            Console.WriteLine("Hello, World!");
+            String input = Console.ReadLine();
+            Console.WriteLine(input);
+
+        }
+    }
+}
